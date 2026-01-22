4c16bc0f5f49e50b5f5cc0db0c73ea2a33a8e1a5
ganeshpariyar99 <ganeshpariyar987@gmail.com>
2026-01-22 12:59:18 +0545

    login and registration

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+// const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+
+require('dotenv').config();
+require('./Models/db');
+
+const PORT = process.env.PORT || 3000;
+
+// app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', AuthRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`)
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend is working ');
+});
